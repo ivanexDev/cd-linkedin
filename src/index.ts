5b9082fe222e5777db88f6d2586713a90d9da660
ivanexDev <ivanexdev@gmail.com>
2025-04-09 16:27:53 -0400

    First commit

---
+++ src/index.ts
@@ -0,0 +1,10 @@
+
+import express, {Request, Response} from 'express'
+
+const app = express();
+
+app.get('/', (req: Request, res: Response) => {
+  res.send('Hello, TypeScript + Node.js!');
+});
+
+app.listen(3000, () => console.log('Server running on port 3000'));
